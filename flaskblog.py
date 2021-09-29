737a9348dc34daecd851511f0a182dfbd70be023
Vaibhav20201 <vaibhav_k@mt.iitr.ac.in>
2021-09-30 03:41:21 +0530

    Add authentication

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,9 @@
-from flask import Flask,render_template,url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegisterationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'This is my Secret Key.'
 
 posts=[
     {
@@ -25,6 +28,25 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+@app.route("/register", methods=['GET','POST'])
+def register():
+    form = RegisterationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for { form.username.data }!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "a@b.c" and form.password.data == "12345678":
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Invalid Credentials! Please try again.', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__=='__main__':
